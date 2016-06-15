853556ca128638bae824a10421609684f07ed7f4
midhun <iammidhunpj@gmail.com>
2016-06-15 16:20:06 +0530

    changes made in the datatable according to comments

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,4 +1,6 @@
-    expenseManager.controller('authController', function($scope, $state) {
+    expenseManager.controller('authController', function($scope, $state, $rootScope) {
+
+        $rootScope.isLoggedIn = false;
         $scope.submitLogin = function(email, password) {
             if (email == "" || password == "" || email == undefined || password == undefined) {
                 $scope.error = true;
@@ -11,9 +13,11 @@
                     if (email == "user@qburst.com" && password == "user") {
                         localStorage.setItem("name", "user");
                         $scope.error = true;
+                        $rootScope.isLoggedIn = true;
                         $state.go('dashboard');
                     } else if (email == "admin@qburst.com" && password == "admin") {
                         localStorage.setItem("name", "admin");
+                        $rootScope.isLoggedIn = true;
                         $state.go('admin');
                     } else {
                         $scope.validationerror = true;
@@ -21,4 +25,6 @@
                 }
             }
         }
+
+
     });
--- app/controllers/expenseController.js
+++ app/controllers/expenseController.js
@@ -1,19 +1,6 @@
-expenseManager.controller('expenseController', function($scope,$state){
-	$scope.logout = true;
+expenseManager.controller('expenseController', function($scope, $state) {
+    $scope.logout = true;
     $scope.openExpense = function() {
-        $state.go('/expense');
+        $state.go('expense');
     }
 });
-
-expenseManager.controller('expenseTableCtrl', expenseTableCtrl);
-function expenseTableCtrl(DTOptionsBuilder, DTColumnBuilder) {
-    var vm = this;
-    vm.dtOptions = DTOptionsBuilder.fromSource('../app/json/data.json')
-        .withPaginationType('full_numbers');
-    vm.dtColumns = [
-        DTColumnBuilder.newColumn('date').withTitle('Date'),
-        DTColumnBuilder.newColumn('purpose').withTitle('Purpose'),
-        DTColumnBuilder.newColumn('description').withTitle('Description'),
-        DTColumnBuilder.newColumn('amount').withTitle('Amount')
-    ];
-}
