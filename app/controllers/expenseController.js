3c18fb19d3c415ea530b3bb1d9833f90e7434966
midhun <iammidhunpj@gmail.com>
2016-06-17 17:31:27 +0530

    expense page validation bug fixed

--- app/controllers/expenseController.js
+++ app/controllers/expenseController.js
@@ -1,9 +1,6 @@
 expenseManager.controller('expenseController', function($scope, $state, userdataService) {
     $scope.locations = ["Trivandrum", "Kochi", "Koratty", "Calicut", "Poland"];
     $scope.purposes = ['Electicity Bill', 'Internet Bill', 'Transportation'];
-    // var singleExpense = $scope.expenses[index];
-    // $scope.date = singleExpense['date'];
-    // $scope.purpose = singleExpense['purpose'];
     $scope.addExpense = function() {
         userdataService.addUserdata($scope.newexpense);
         $scope.newexpense = {};
